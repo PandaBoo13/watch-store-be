c7f33376be00279fe2cafc4394e7e4b80513814e
PandaBoo <thuyenle1307@gmail.com>
2025-04-27 19:43:46 +0700

    new

---
+++ src/test/java/com/example/watch_store_be/WatchStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watch_store_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchStoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
