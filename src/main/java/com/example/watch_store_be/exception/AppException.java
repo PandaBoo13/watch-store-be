22acc4e98dc2e739b4f93469defbb8ea9965cd3d
PandaBoo <thuyenle1307@gmail.com>
2025-05-02 19:41:42 +0700

    not spring

--- src/main/java/com/example/watch_store_be/exception/AppException.java
+++
@@ -1,7 +0,0 @@
-package com.example.watch_store_be.exception;
-
-public class AppException extends RuntimeException {
-  public AppException(String message) {
-    super(message);
-  }
-}
--- src/main/java/com/example/watch_store_be/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.watch_store_be.repository;
-
-import com.example.watch_store_be.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account,String> {
-}
