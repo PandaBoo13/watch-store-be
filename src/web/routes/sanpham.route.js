c2fad469034f868bd0805af5c3c6f00a7ccf48a6
PandaBoo <thuyenle1307@gmail.com>
2025-06-15 18:34:46 +0700

    sanpham

---
+++ src/web/routes/sanpham.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const SanPhamController = require("../controllers/SanPhamController");
+
+// Tạo sản phẩm mới
+router.post("/create", SanPhamController.create);
+
+// Xóa sản phẩm theo ID
+router.delete("/delete/:id", SanPhamController.delete);
+
+// Lấy tất cả sản phẩm
+router.get("/", SanPhamController.getAll);
+
+// Lấy sản phẩm theo ID
+router.get("/getId/:id", SanPhamController.getById);
+
+// Cập nhật sản phẩm
+router.put("/update/:id", SanPhamController.update);
+
+module.exports = router;
